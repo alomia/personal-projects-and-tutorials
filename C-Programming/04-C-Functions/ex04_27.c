cbef86ec2b10ccc7e53defe634910b66895976b6
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-01-12 12:27:48 -0500

    C function

---
+++ C-Programming/04-C-Functions/ex04_27.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+        int c; // variable to hold character input by user
+        if ((c = getchar()) != EOF) {
+                main();
+                printf("%c", c);
+        }
+}
+
