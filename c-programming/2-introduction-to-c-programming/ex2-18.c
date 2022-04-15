f09f92239aa1b0d93151148ff81751a5771e843a
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-04-15 17:22:50 -0500

    Comparing Integers

---
+++ c-programming/2-introduction-to-c-programming/ex2-18.c
@@ -0,0 +1,26 @@
+// Chapter 2 - Introduction to C Programming
+// Exercise 2.18 - Comparing Integers
+#include <stdio.h>
+
+int main(void)
+{
+    int firstNumber, secondNumber = 0;
+
+    printf("%s", "Enter two integers: ");
+    scanf("%d %d", &firstNumber, &secondNumber);
+
+    if (firstNumber > secondNumber)
+    {
+        printf("%d%s\n", firstNumber, " is large");
+    }
+
+    if (secondNumber > firstNumber)
+    {
+        printf("%d%s\n", secondNumber, " is large");
+    }
+
+    if (firstNumber == secondNumber)
+    {
+        printf("%s\n", "These numbers are equal");
+    }
+}
