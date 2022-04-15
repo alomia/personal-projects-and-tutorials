2107b8ffcf6866839a4dcba99dadfee8ceb14e9f
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-04-15 18:32:57 -0500

    Shapes with Asterisks

---
+++ c-programming/2-introduction-to-c-programming/ex2-21.c
@@ -0,0 +1,16 @@
+// Chapter 2 - Introduction to C Programming
+// Exercise 2.20 - Diameter, Circumference and Area of a Circle
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%s\n", "*********          ***            *             *");
+    printf("%s\n", "*       *        *     *         ***           * *");
+    printf("%s\n", "*       *       *       *       *****         *   *");
+    printf("%s\n", "*       *       *       *         *          *     *");
+    printf("%s\n", "*       *       *       *         *         *       *");
+    printf("%s\n", "*       *       *       *         *          *     *");
+    printf("%s\n", "*       *       *       *         *           *   *");
+    printf("%s\n", "*       *        *     *          *            * *");
+    printf("%s\n", "*********          ***            *             *");
+}
