d6a24b2b394747a87430bdae476aa8aa4501abf5
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-04-28 00:12:16 -0500

    Validating User Input

---
+++ c-programming/3-structured-program-development-in-c/ex3-27.c
@@ -0,0 +1,42 @@
+// Chapter 3 - Structured Program Development in C
+// Exercise 3.27 - Validating User Input
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned int passes = 0;
+    unsigned int failures = 0;
+    unsigned int student = 1;
+    int result;
+
+    while (student <= 10)
+    {
+        printf("%s", "Enter result ( 1=pass,2=fail ): ");
+        scanf("%d", &result);
+
+        if (result != 1 && result != 2)
+        {
+            puts("Please enter a correct value");
+            student = student;
+            continue;
+        }
+
+        if (result == 1)
+        {
+            passes = passes + 1;
+        }
+        else if (result == 2)
+        {
+            failures = failures + 1;
+        }
+        student = student + 1;
+    }
+
+    printf("Passed %u\n", passes);
+    printf("Failed %u\n", failures);
+
+    if (passes > 8)
+    {
+        puts("Bonus to instructor!");
+    }
+}
